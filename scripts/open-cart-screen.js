b417058811aa3671d57436d15e3b5b0a25689a4f
Tyler White <7670501@isd535.org>
2022-04-18 11:15:24 -0500

    added total

--- scripts/open-cart-screen.js
+++ scripts/open-cart-screen.js
@@ -1,4 +1,4 @@
-var total;
+var total = 0;
 const CART_DATA_LOC = 'ABCabc15739';
 
 function makeDivsOfOrder() {
@@ -14,13 +14,13 @@ function makeDivsOfOrder() {
         let $productName = $('<td class="prod-title"></td>');
         $productName.html(i);
         let $productPricePer = $('<td class="prod-price-per"></td>');
-        $productPricePer.html(itemPrices[i]);
+        $productPricePer.html('$' + itemPrices[i]);
         let $productCount = $('<td class="prod-num"></td>');
         $productCount.html(cartItems[i]);
         let $productLineTotal = $('<td class="prod-tot-price"></td>');
         let prlt = itemPrices[i] * cartItems[i];
         total += prlt;
-        $productLineTotal.html(prlt);
+        $productLineTotal.html('$' + prlt);
         $itemDis.append($productName);
         console.log($productCount + $productLineTotal + $productName + $productPricePer);
         $itemDis.append($productPricePer);
@@ -28,10 +28,18 @@ function makeDivsOfOrder() {
         $itemDis.append($productLineTotal);
         $orderCont.append($itemDis);
     }
+    let $footerRow = $('<tr class="item-display"></tr>');
+    $footerRow.append($('<td class="prod-title"></td>'));
+    $footerRow.append($('<td class="prod-price-per"></td>'));
+    $footerRow.append($('<th class="prod-num">Total:</th>'));
+    let $finalLine = $('<th class="prod-tot-price"></th>');
+    $finalLine.html('$' + total);
+    $footerRow.append($finalLine);
+    $orderCont.append($footerRow);
     $('#checkout-items-container').append($orderCont);
 }
 
-function displayOrder() {
+function displayOrder() { //gets order data to put on screen
     'use strict';
     let theDataItself = {};
     remoteDataStore.get(CART_DATA_LOC, function (resp) {
@@ -39,6 +47,16 @@ function displayOrder() {
         cartItems = JSON.parse(theDataItself['pdata']);
         itemPrices = JSON.parse(theDataItself['sdata']);
         makeDivsOfOrder();
+    });
+}
+
+function getVariables() { //i would just change the function above but that tends to break everything so here is a copy
+    'use strict';
+    let theDataItself = {};
+    remoteDataStore.get(CART_DATA_LOC, function (resp) {
+        theDataItself = resp;;
+        cartItems = JSON.parse(theDataItself['pdata']);
+        itemPrices = JSON.parse(theDataItself['sdata']);
     });
 }
 
@@ -52,6 +70,6 @@ function openCartScreen() {
 
 function openShopScreen() {
     //remoteDataStore.remove(CART_DATA_LOC, function () {});
-    remoteDataStore.add({'emailAddress': CART_DATA_LOC, 'pdata': cartItems, 'sdata': itemPrices});
+    remoteDataStore.add({'emailAddress': CART_DATA_LOC, 'pdata': JSON.stringify(cartItems), 'sdata': JSON.stringify(itemPrices)});
     window.open('index.html', '_self');
 }

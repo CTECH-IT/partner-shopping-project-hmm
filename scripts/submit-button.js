473c445713593ebf8783674a0045418f2636c5a9
Caleb Pardeik <7912201@isd535.org>
2022-04-19 10:52:22 -0500

    email now sees if theer is an e-mail address listed

--- scripts/submit-button.js
+++ scripts/submit-button.js
@@ -1,7 +1,14 @@
 
 document.getElementById("submit-button").addEventListener("click", function(event) {
     event.preventDefault();
-    email = document.getElementById('email-input').value
-    remoteDataStore.add({'emailAddress': email, 'pdata': JSON.stringify(cartItems), 'sdata': JSON.stringify(itemPrices)});
-    console.log(cartItems)
+    
+    if (document.getElementById('email-input').value) {
+        email = document.getElementById('email-input').value;
+        remoteDataStore.add({'emailAddress': email, 'factor': 'calebtyler', 'pdata': JSON.stringify(cartItems), 'sdata': JSON.stringify(itemPrices)});
+        console.log(cartItems);
+        remoteDataStore.remove('ABCabc15739');
+        remoteDataStore.add({'emailAddress': 'ABCabc15739', 'pdata': {}, 'sdata': {}});
+    } else {
+        console.log("ji")
+    }
 })

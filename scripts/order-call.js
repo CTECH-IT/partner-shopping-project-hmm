b7e3fb47d9861fe8de89978493b92a19078322cb
Tyler White <7670501@isd535.org>
2022-04-19 10:54:44 -0500

    wip

---
+++ scripts/order-call.js
@@ -0,0 +1,67 @@
+(function (window) { //copy of coffee run checklist maker(ish)
+    'use strict';
+    let App = window.App || {};
+    let $ = window.jQuery;
+    function CheckList(selector) {
+        if (!selector) {
+            throw new Error('No selector provided');
+        }
+        this.$element = $(selector);
+        if (this.$element.length === 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+    }
+
+    function Row(coffeeOrder) {
+        let $div = $('<div></div>', 
+            {'data-coffee-order': 'checkbox', 
+            'class': 'checkbox'});
+        let $label = $('<label></label>');
+        let $checkbox = $('<input></input>',
+            {type: 'checkbox', 
+            value: coffeeOrder.emailAddress});
+        
+        let description = ' ' + coffeeOrder.size;
+        if (coffeeOrder.flavor) {
+            description += ' ' + coffeeOrder.flavor;
+        }
+        description += ' ' + coffeeOrder.coffee + ',';
+        description += ' (' + coffeeOrder.emailAddress + ')';
+        description += ' [' + coffeeOrder.strength + 'x]';
+
+        $label.append($checkbox);
+        $label.append(description);
+        $div.append($label);
+        this.$element = $div;
+    }
+
+    CheckList.prototype.removeRow = function (email) {
+        this.$element
+            .find('[value="' + email + '"]')
+            .closest('[data-coffee-order="checkbox"]')
+            .remove();
+    };
+    
+    CheckList.prototype.addRow = function (coffeeOrder) {
+        this.removeRow(coffeeOrder.emailAddress);
+        var rowElement = new Row(coffeeOrder);
+        this.$element.append(rowElement.$element);
+    };
+
+    CheckList.prototype.addClickHandler = function (func) {
+        this.$element.on('click', 'input', function (event) {
+            var email = event.target.value;
+            this.removeRow(email);
+            func(email);
+        }.bind(this));
+    };
+
+    App.CheckList = CheckList;
+    window.App = App;
+})(window);
+
+var orderList = new CheckList('#order-container');
+
+function makeChecklist() {
+    
+}
